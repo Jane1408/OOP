fc1e20748bb86347a36954a7a54cc0577c529e63
Женечка <>
2016-03-19 10:42:53 +0300

    исправления в main()

--- Lab2/FindAndReplace/FindAndReplace/main.cpp
+++ Lab2/FindAndReplace/FindAndReplace/main.cpp
@@ -10,13 +10,16 @@ int main(int argc, char *argv[])
 	if (argc == 3)
 	{
 		std::string subject;
+		std::string search = argv[1];
+		std::string replace = argv[2];
+
 		while (std::getline(std::cin, subject))
 		{
 			if (subject.empty())
 			{
 				break;
 			}
-			std::cout << FindAndReplace(subject, argv[1], argv[2]) << std::endl;
+			std::cout << FindAndReplace(subject, search, replace) << std::endl;
 		}
 	}
 	return 0;
